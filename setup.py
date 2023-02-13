349da7825884ce0f45bcb84617d3fb1474a4f576
Luchesar ILIEV <luchesar.iliev@gmail.com>
2023-02-13 09:12:11 +0200

    ops: prepare for PyPI

--- setup.py
+++ setup.py
@@ -7,16 +7,67 @@ import sys
 
 from setuptools import setup, find_packages, Command
 
+NAME = 'oranchada'
+
+VERSION = '0.0.1'
+
+AUTHOR = 'IDEAconsult Ltd.'
+AUTHOR_EMAIL = 'dev-charisma@ideaconsult.net'
+
+URL = 'https://github.com/h2020charisma/ramanchada-orange'
+DESCRIPTION = 'Orange add-on for Raman spectroscopy'
+LONG_DESCRIPTION = open(
+    path.join(path.dirname(__file__), 'README.pypi'), 'r', encoding='utf-8'
+).read()
+
+LICENSE = 'MIT'
+
+KEYWORDS = (
+    # [PyPi](https://pypi.python.org) packages with keyword "orange3 add-on"
+    # can be installed using the Orange Add-on Manager
+    'Raman',
+    'oranchada',
+    'orange3 add-on',
+    'spectroscopy',
+)
+
 PACKAGES = find_packages()
 
 PACKAGE_DATA = {}
 
-README_FILE = os.path.join(os.path.dirname(__file__), 'README.pypi')
-LONG_DESCRIPTION = open(README_FILE).read()
-
 DATA_FILES = [
     # Data files that will be installed outside site-packages folder
 ]
+
+INSTALL_REQUIRES = [
+    'Orange3>=3.31.0',
+    'numpy>=1.18.0',
+    'orange-canvas-core>=0.1.24',
+    'orange-widget-base>=4.16.1',
+    'pip>=9.0',  # same as for Orange 3.28
+    'ramanchada2 @ git+https://github.com/h2020charisma/ramanchada2.git',
+    'setuptools>=36.3',  # same as for Orange 3.28
+    #'AnyQt>=0.0.6',
+    #'bottleneck',
+    #'colorcet',
+    #'extranormal3 >=0.0.3',
+    #'h5py',
+    #'lmfit>=1.0.2',
+    #'pebble',
+    #'pillow',
+    #'pyqtgraph>=0.11.1,!=0.12.4',  # https://github.com/pyqtgraph/pyqtgraph/issues/2237
+    #'renishawWiRE>=0.1.8',
+    #'scikit-learn>0.23.0',
+    #'scipy>=1.4.0',
+    #'serverfiles>=0.2',
+    #'spectral>=0.18,!=0.23',
+]
+
+EXTRAS_REQUIRE = {
+    'test': ['coverage'],
+    #'test': ['pytest', 'coverage'],
+    #'doc': ['sphinx', 'recommonmark', 'sphinx_rtd_theme'],
+}
 
 ENTRY_POINTS = {
     # Entry points that marks this package as an orange add-on. If set, addon will
@@ -46,16 +97,27 @@ ENTRY_POINTS = {
 
 }
 
-KEYWORDS = [
-    # [PyPi](https://pypi.python.org) packages with keyword "orange3 add-on"
-    # can be installed using the Orange Add-on Manager
-    'orange3 add-on',
-    'spectroscopy',
-    'Raman'
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Environment :: Plugins',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Education',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3 :: Only',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'Topic :: Scientific/Engineering :: Bio-Informatics',
+    'Topic :: Scientific/Engineering :: Visualization',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: Software Development :: Libraries',
 ]
 
-
 TEST_SUITE = "orangecontrib.charisma.tests.suite"
+
 
 def include_documentation(local_dir, install_dir):
     global DATA_FILES
@@ -65,6 +127,7 @@ def include_documentation(local_dir, install_dir):
         doc_files.append((dirpath.replace(local_dir, install_dir),
                           [path.join(dirpath, f) for f in files]))
     DATA_FILES.extend(doc_files)
+
 
 if __name__ == '__main__':
 
@@ -76,50 +139,24 @@ if __name__ == '__main__':
     #include_documentation('doc/build/htmlhelp', 'help/orange-spectroscopy')
 
     setup(
-        name="charisma",
-        python_requires='>3.8.0',
-        description='Extends Orange with Raman spectroscopy',
+        author=AUTHOR,
+        author_email=AUTHOR_EMAIL,
+        classifiers=CLASSIFIERS,
+        data_files=DATA_FILES,
+        description=DESCRIPTION,
+        entry_points=ENTRY_POINTS,
+        extras_require=EXTRAS_REQUIRE,
+        include_package_data=True,
+        install_requires=INSTALL_REQUIRES,
+        keywords=KEYWORDS,
+        license=LICENSE,
         long_description=LONG_DESCRIPTION,
         long_description_content_type='text/markdown',
-        author='Ideaconsult Ltd.',
-        author_email='dev-charisma@ideaconsult.net',
-        version="0.0.1",
-        packages=PACKAGES,
-        package_data=PACKAGE_DATA,
-        data_files=DATA_FILES,
-        install_requires=[
-            'setuptools>=36.3',  # same as for Orange 3.28
-            'pip>=9.0',  # same as for Orange 3.28
-            'numpy>=1.18.0',
-            'Orange3>=3.31.0',
-            'orange-canvas-core>=0.1.24',
-            'orange-widget-base>=4.16.1',
-            #'scipy>=1.4.0',
-            #'scikit-learn>0.23.0',
-            #'spectral>=0.18,!=0.23',
-            #'serverfiles>=0.2',
-            #'AnyQt>=0.0.6',
-            #'pyqtgraph>=0.11.1,!=0.12.4',  # https://github.com/pyqtgraph/pyqtgraph/issues/2237
-            #'colorcet',
-            #'h5py',
-            #'extranormal3 >=0.0.3',
-            #'renishawWiRE>=0.1.8',
-            #'pillow',
-            #'lmfit>=1.0.2',
-            #'bottleneck',
-            #'pebble',
-            'ramanchada2'
-        ],
-        extras_require={
-            'test': ['coverage']
-        },
-        entry_points=ENTRY_POINTS,
-        keywords=KEYWORDS,
+        name=NAME,
         namespace_packages=['orangecontrib'],
-        #test_suite=TEST_SUITE,
-        include_package_data=True,
+        package_data=PACKAGE_DATA,
+        packages=PACKAGES,
+        url=URL,
+        version=VERSION,
         zip_safe=False,
-        url="https://github.com/h2020charisma/ramanchada-orange",
-        cmdclass=cmdclass,
-        license='MIT',
     )
