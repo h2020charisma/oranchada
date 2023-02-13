3417140759bfed592a6a08e8654ba61c392789de
Luchesar ILIEV <luchesar.iliev@gmail.com>
2023-02-13 12:47:55 +0200

    ops: prepare for PyPI 2

--- setup.py
+++ setup.py
@@ -5,27 +5,27 @@ from setuptools import find_packages, setup
 
 NAME = 'oranchada'
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 
+DESCRIPTION = 'Orange add-on for Raman spectroscopy'
+README_FILE = path.join(path.dirname(__file__), 'README.pypi')
+LONG_DESCRIPTION = open(README_FILE, 'r', encoding='utf-8').read()
+LONG_DESCRIPTION_CONTENT_TYPE = 'text/markdown'
+URL = 'https://github.com/h2020charisma/ramanchada-orange'
 AUTHOR = 'IDEAconsult Ltd.'
 AUTHOR_EMAIL = 'dev-charisma@ideaconsult.net'
-
-URL = 'https://github.com/h2020charisma/ramanchada-orange'
-DESCRIPTION = 'Orange add-on for Raman spectroscopy'
-LONG_DESCRIPTION = open(
-    path.join(path.dirname(__file__), 'README.pypi'), 'r', encoding='utf-8'
-).read()
-
 LICENSE = 'MIT'
 
-KEYWORDS = (
+KEYWORDS = [
     # [PyPi](https://pypi.python.org) packages with keyword "orange3 add-on"
     # can be installed using the Orange Add-on Manager
     'Raman',
     'oranchada',
     'orange3 add-on',
     'spectroscopy',
-)
+]
+
+PYTHON_REQUIRES = '>3.8.0'
 
 PACKAGES = find_packages()
 
@@ -41,7 +41,7 @@ INSTALL_REQUIRES = [
     'orange-canvas-core>=0.1.24',
     'orange-widget-base>=4.16.1',
     'pip>=9.0',  # same as for Orange 3.28
-    'ramanchada2 @ git+https://github.com/h2020charisma/ramanchada2.git',
+    'ramanchada2>=0.0.1',
     'setuptools>=36.3',  # same as for Orange 3.28
     # 'AnyQt>=0.0.6',
     # 'bottleneck',
@@ -125,12 +125,11 @@ def include_documentation(local_dir, install_dir):
     DATA_FILES.extend(doc_files)
 
 
-if __name__ == '__main__':
-
-    cmdclass = {
-        # 'coverage': CoverageCommand,
-        # 'lint': LintCommand,
-    }
+def setup_package():
+    # cmdclass = {
+    #     'coverage': CoverageCommand,
+    #     'lint': LintCommand,
+    # }
 
     # include_documentation('doc/build/htmlhelp', 'help/orange-spectroscopy')
 
@@ -147,12 +146,17 @@ if __name__ == '__main__':
         keywords=KEYWORDS,
         license=LICENSE,
         long_description=LONG_DESCRIPTION,
-        long_description_content_type='text/markdown',
+        long_description_content_type=LONG_DESCRIPTION_CONTENT_TYPE,
         name=NAME,
-        namespace_packages=['orangecontrib'],
+        namespace_packages=["orangecontrib"],
         package_data=PACKAGE_DATA,
         packages=PACKAGES,
+        python_requires=PYTHON_REQUIRES,
         url=URL,
         version=VERSION,
         zip_safe=False,
     )
+
+
+if __name__ == '__main__':
+    setup_package()
