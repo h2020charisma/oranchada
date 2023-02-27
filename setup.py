ce3118db8fb5be6aa8059c833a73f1318c5613b2
Georgi Georgiev <georgiev.georgi.st@gmail.com>
2023-02-27 16:36:49 +0200

    v0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 NAME = 'oranchada'
 
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 
 DESCRIPTION = 'Orange add-on for Raman spectroscopy'
 README_FILE = path.join(path.dirname(__file__), 'README.pypi')
