a3b613d064ac3e2d9638a4ce0550cb973aeb8ac9
Luchesar ILIEV <luchesar.iliev@gmail.com>
2023-05-27 12:23:13 +0300

    prepare v0.0.11

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 NAME = 'oranchada'
 
-VERSION = '0.0.10'
+VERSION = '0.0.11'
 
 DESCRIPTION = 'Orange add-on for Raman spectroscopy'
 README_FILE = path.join(path.dirname(__file__), 'README.pypi')
