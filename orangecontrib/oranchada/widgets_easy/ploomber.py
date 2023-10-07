f6c0e8a50551b043b80c3e2424679d5dc6c28499
vedina <jeliazkova.nina@gmail.com>
2023-10-07 22:24:10 +0300

    spectrasets as input

--- orangecontrib/oranchada/widgets_easy/ploomber.py
+++ orangecontrib/oranchada/widgets_easy/ploomber.py
@@ -14,6 +14,8 @@ from ploomber.spec import DAGSpec
 from ploomber import DAG
 import os
 import yaml
+from ..base_widget import FilterWidget
+from ..base_widget import BaseWidget, RC2Spectra
 
 for handler in logging.root.handlers[:]:
     logging.root.removeHandler(handler)
@@ -23,7 +25,7 @@ log = logging.getLogger("charisma")
 log.info("log hijack for debugging")
 
 
-class PredefinedPloomberWidget(OWWidget):
+class PredefinedPloomberWidget(BaseWidget):
     # Define the widget's name, category, and outputs
     name = "Ploomber Workflow Runner"
     description = "Execute predefined Ploomber workflows with YAML and environment files."
@@ -37,15 +39,19 @@ class PredefinedPloomberWidget(OWWidget):
     yaml_file = os.path.join(os.path.dirname(__file__), "ploomber","workflow_twinning", "pipeline.yaml")
     env_file = os.path.join(os.path.dirname(__file__), "ploomber", "workflow_twinning","env.yaml")
 
+    should_auto_proc = Setting(False)
 
     class Inputs:
-        # specify the name of the input and the type
-        data = Input("Data", Table)
-
+        reference_spe = Input("Reference (RC2Spectra)", RC2Spectra, default=True)
+        twinned_spe = Input("Twinned (RC2Spectra)", RC2Spectra, default=False)
+        reference_led = Input("Reference LED (RC2Spectra)", RC2Spectra, default=True)
+        twinned_led = Input("Twinned LED (RC2Spectra)", RC2Spectra, default=False)      
+        data = Input("Data", Table, default=False)   
 
     class Outputs:
-        # if there are two or more outputs, default=True marks the default output
-        data_out = Output("Processed Data", Table, default=True)
+        reference_spe = Output("Reference (RC2Spectra)", RC2Spectra, default=True)
+        twinned_spe = Output("Twinned (RC2Spectra)", RC2Spectra, default=True)
+               
 
     # same class can be initiated for Error and Information messages
     class Warning(OWWidget.Warning):
@@ -86,6 +92,16 @@ class PredefinedPloomberWidget(OWWidget):
             self.env_file = filenames[0]
             self.env2table()   
 
+    def on_finish(self,dag,report):
+        print(dag)
+        print(report)
+
+    def on_render(self,dag):
+        print("render ",dag)
+
+    def on_failure(self,dag,report):
+        print(report)
+
     def load_workflow(self):
         if not self.yaml_file or not self.env_file:
             self.statusBar().showMessage("Please select both YAML and environment files.")
@@ -93,7 +109,9 @@ class PredefinedPloomberWidget(OWWidget):
         try:
             self.dag_spec = DAGSpec(data= self.yaml_file, env = self.env)
             self.dag = self.dag_spec.to_dag()
-            self.dag.on_finish(lambda dag,report: print(report))                   
+            self.dag.on_finish = self.on_finish
+            self.dag.on_render = self.on_render
+
             log.info(self.dag.status())
            
             self.statusBar().showMessage("Workflow loaded successfully.")
@@ -112,6 +130,42 @@ class PredefinedPloomberWidget(OWWidget):
         except Exception as e:
             log.info(e)
             self.statusBar().showMessage(f"Error: {str(e)}")            
+
+    @Inputs.reference_spe
+    def set_reference_spe(self, spe):
+        if spe:
+            self.reference_spe = spe
+        else:
+            self.reference_spe = None
+        self.update_inputs_info()
+        self.auto_process()
+
+    @Inputs.twinned_spe
+    def set_twinned_spe(self, spe):
+        if spe:
+            self.twinned_spe = spe
+        else:
+            self.twinned_spe = None
+        self.update_inputs_info()
+        self.auto_process()     
+
+    @Inputs.reference_led
+    def set_reference_led(self, spe):
+        if spe:
+            self.reference_led = spe
+        else:
+            self.reference_led = None
+        self.update_inputs_info()
+        self.auto_process()
+
+    @Inputs.twinned_led
+    def set_twinned_spe(self, spe):
+        if spe:
+            self.twinned_led = spe
+        else:
+            self.twinned_led = None
+        self.update_inputs_info()
+        self.auto_process()            
 
     @Inputs.data
     def set_data(self, data):
